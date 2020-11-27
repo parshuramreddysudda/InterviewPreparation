0cb0861c318c24d2bdf214608d3b2c5477e8fca5
Parshuarm Reddy <parshuram.sudda@gmail.com>
2020-11-27 22:43:33 +0530

    Update EqualSubSet.java

--- GeeksForGeekPractice/DynamicProgramming/EqualSubSet.java
+++ GeeksForGeekPractice/DynamicProgramming/EqualSubSet.java
@@ -1,10 +1,12 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class EqualSubSet {
 
 	public static void main(String[] args) {
 
-		int arr[] = { 1, 3, 5,3 };
+		int arr[] = { 1,2,5 };
 		int sum = 0;
 		for (int s : arr) {
 			sum += s;
@@ -12,15 +14,46 @@ public class EqualSubSet {
 		if (sum % 2 != 0)
 			System.out.println("Not found");
 		else {
-			Boolean[][] dp = new Boolean[arr.length][sum + 1];
-			System.out.println(subSet(dp, arr, 0, 0, sum / 2));
+			boolean[][] dp = new boolean[arr.length][sum + 1];
+//			System.out.println(subSet(dp, arr, 0, 0, sum / 2));
+			System.out.print(subsetDP(dp, arr, sum / 2));
+
+//			for (int i = 0; i < dp.length; i++) {
+//				for (int j = 0; j < dp[i].length; j++)
+//					System.out.print(dp[i][j] + " ");
+//				System.out.println();
+//			}
 		}
 
 	}
 
+	private static boolean subsetDP(boolean[][] dp, int[] arr, int sum) {
+
+		for (int i = 0; i < dp.length; i++)
+			dp[i][0] = true;
+
+		 for(int s=1; s <= sum ; s++) {
+		      dp[0][s] = (arr[0] == s ? true : false);
+		    }
+
+
+		for (int i = 1; i < dp.length; i++) {
+			for (int s = 1; s <=sum; s++) {
+
+				if (dp[i - 1][s]) {
+					dp[i][s] = dp[i - 1][s];
+				} else if (s >= arr[i]) {
+					dp[i][s] = dp[i - 1][s - arr[i]];
+				}
+
+			}
+		}
+		return dp[arr.length - 1][sum];
+	}
+
+	@SuppressWarnings("unused")
 	private static boolean subSet(Boolean[][] dp, int[] arr, int index, int sum, int total) {
 
-		System.out.println("SUm is " + sum);
 		if (sum > total || index >= arr.length) {
 			return false;
 		}
@@ -35,6 +68,7 @@ public class EqualSubSet {
 
 		return dp[index][sum];
 	}
+
 	@SuppressWarnings("unused")
 	private static boolean subSetOneDimension(Boolean[] dp, int[] arr, int index, int sum, int total) {
 
