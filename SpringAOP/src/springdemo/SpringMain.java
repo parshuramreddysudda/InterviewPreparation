199cc31384a24c842fb903116a08ad59c5bfbee9
Parshuarm Reddy <parshuram.sudda@gmail.com>
2021-04-30 18:45:41 +0530

    Update SpringMain.java

--- SpringAOP/src/springdemo/SpringMain.java
+++ SpringAOP/src/springdemo/SpringMain.java
@@ -3,15 +3,12 @@ package springdemo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class SpringDemo {
-
+public class SpringMain {
 	public static void main(String[] args) {
-
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		StudentDatabase stu = context.getBean(StudentDatabase.class);
 		stu.getDetails();
 		System.out.println("================================================");
 		stu.getStudent(3);
 	}
-
 }
