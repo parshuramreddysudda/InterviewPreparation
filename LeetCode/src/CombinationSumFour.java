af914f7c1582a3ebd6a1b2396c60e07e04553ffa
parshuramreddysudda <parshuram.sudda@gmail.com>
2022-05-11 16:43:14 +0530

    Update CombinationSumFour.java

--- LeetCode/src/CombinationSumFour.java
+++ LeetCode/src/CombinationSumFour.java
@@ -3,31 +3,36 @@ import java.util.List;
 
 public class CombinationSumFour {
     public static void main(String[] args) {
-        int target = 10;
-        System.out.println(combinationSum4(new int[]{4, 2, 1}, target));
+        int target = 4;
+        System.out.println(combinationSum4(new int[]{1,2,3}, target));
+        System.out.println("Sum value is "+sum);
     }
-
+static int  sum=0;
     public static int combinationSum4(int[] nums, int target) {
 
         if (nums.length == 0)
             return 0;
 
-        return backTrackSolution(target, nums, 0);
+        return backTrackSolution(new ArrayList<>(),target, nums, 0);
     }
 
-    public static int backTrackSolution(int targetSum, int[] candidates, int count) {
+    public static int backTrackSolution(List<Integer> tempList,int targetSum, int[] nums, int count) {
 
 
         if (targetSum < 0)
             return 0;
 
         if (targetSum == 0) {
-            return count + 1;
+            sum+=1;
+            System.out.println(tempList);
+            return 1;
         }
 
-        for (int i = 0; i < candidates.length; i++) {
-            count += backTrackSolution(targetSum - candidates[i], candidates, count);
-            System.out.println(" Target is" + targetSum + " TempList size is " + " Count is " + count);
+        for (int i = 0; i < nums.length; i++) {
+//            tempList.add(nums[i]);
+            System.out.println(" Target is" + targetSum + " Count is " + count);
+            count += backTrackSolution(tempList,targetSum - nums[i], nums, count);
+//            tempList.remove(tempList.size()-1);
         }
         return count;
 
