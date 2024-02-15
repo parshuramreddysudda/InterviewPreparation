c70c0d5915febfae4a20cd07be8d1691a98a5b8e
Parshuram Reddy <parshuram.sudda@gmail.com>
2024-02-14 23:07:42 -0500

    Create isBadVersion.py

---
+++ Python Interview Preparation/Educative/pythonProject/BinarySearch/isBadVersion.py
@@ -0,0 +1,30 @@
+# -- DO NOT CHANGE THIS SECTION -----------------
+
+# import main as api_call
+
+
+def is_bad_version(
+        v):  # is_bad_version() is the API function that returns true or false depending upon whether the provided version ID is bad or not
+    return True
+    # return api_call.is_bad(v)
+
+
+# -----------------------------------------------
+
+def first_bad_version(n):
+    # Replace this placeholder return statement with your code
+
+    low = 0
+    high = n
+
+    while low < high:
+        mid = low + (high - low) // 2
+        if is_bad_version(mid)
+            high = mid
+        else:
+            low = mid
+
+    return 0, 0
+
+
+
