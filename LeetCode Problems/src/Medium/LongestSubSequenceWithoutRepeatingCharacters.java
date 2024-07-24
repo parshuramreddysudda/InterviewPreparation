09f1989438c377f8b71053c2a3c867bef952341c
Parshuram Reddy <parshuram.sudda@gmail.com>
2024-07-24 19:23:06 -0400

    Update LongestSubSequenceWithoutRepeatingCharacters.java

--- LeetCode Problems/src/Medium/LongestSubSequenceWithoutRepeatingCharacters.java
+++ LeetCode Problems/src/Medium/LongestSubSequenceWithoutRepeatingCharacters.java
@@ -1,29 +1,35 @@
 package Medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.HashSet;
 
 class LongestSubSequenceWithoutRepeatingCharacters {
     public static void main(String[] args) {
-        System.out.println(longestSubSequenceWithoutRepeatingCharacters("abcabcaacbd"));
+        System.out.println(longestSubSequenceWithoutRepeatingCharacters("pwwkew"));
     }
     public static int longestSubSequenceWithoutRepeatingCharacters(String s) {
 
-        if(s.length()==0||s.length()==1){
+        if(s.isEmpty() ||s.length()==1){
             return s.length();
         }
-        HashSet<Character> set = new HashSet<>();
+//        HashMap<Character,Integer> set = new HashMap<>();
         int size = 0;
+        Deque<Character> deque= new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
-            if (set.contains(s.charAt(i))) {
-                size = Math.max(size, set.size());
-                while(set.contains(s.charAt(i))){
-                    set.remove(0);
+
+            if(deque.contains(s.charAt(i))){
+                size=Math.max(size,deque.size());
+                while(deque.contains(s.charAt(i))){
+                    deque.removeFirst();
                 }
-                set.add(s.charAt(i));
-            } else {
-                set.add(s.charAt(i));
+                deque.addLast(s.charAt(i));
+            }else {
+                deque.addLast(s.charAt(i));
             }
+
         }
-        return size;
+        return Math.max(size,deque.size());
     }
 }
