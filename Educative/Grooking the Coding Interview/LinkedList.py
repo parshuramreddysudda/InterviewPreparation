fbcd4cc91338391abd4ebdd228536dbd181958b9
Parshuram Reddy <parshuram.sudda@gmail.com>
2024-12-28 16:32:04 -0600

    Create LinkedList.py

---
+++ Educative/Grooking the Coding Interview/LinkedList.py
@@ -0,0 +1,55 @@
+# Template for the linked list
+from LinkedListNode import LinkedListNode
+
+
+class LinkedList:
+    # __init__ will be used to make a LinkedList type object.
+    def __init__(self):
+        self.head = None
+
+    # insert_node_at_head method will insert a LinkedListNode at head
+    # of a linked list.
+    def insert_node_at_head(self, node):
+        if self.head:
+            node.next = self.head
+            self.head = node
+        else:
+            self.head = node
+
+    # create_linked_list method will create the linked list using the
+    # given integer array with the help of InsertAthead method.
+    def create_linked_list(self, lst):
+        for x in reversed(lst):
+            new_node = LinkedListNode(x)
+            self.insert_node_at_head(new_node)
+
+    # returns the number of nodes in the linked list
+    def get_length(self, head):
+        temp = head
+        length = 0
+        while (temp):
+            length += 1
+            temp = temp.next
+        return length
+
+    # returns the node at the specified position(index) of the linked list
+    def get_node(self, head, pos):
+        if pos != -1:
+            p = 0
+            ptr = head
+            while p < pos:
+                ptr = ptr.next
+                p += 1
+            return ptr
+
+    # __str__(self) method will display the elements of linked list.
+    def __str__(self):
+        result = ""
+        temp = self.head
+        while temp:
+            result += str(temp.data)
+            temp = temp.next
+            if temp:
+                result += ", "
+        result += ""
+        return result
