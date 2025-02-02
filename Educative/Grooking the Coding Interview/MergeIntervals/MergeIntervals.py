875f9ab4d65d633c6c59210588ed95f29a27c024
Parshuram Reddy <parshuram.sudda@gmail.com>
2025-02-02 01:32:42 -0500

    Update MergeIntervals.py

--- Educative/Grooking the Coding Interview/MergeIntervals/MergeIntervals.py
+++ Educative/Grooking the Coding Interview/MergeIntervals/MergeIntervals.py
@@ -11,8 +11,7 @@ def merge_intervals(intervals: List[List[int]]) -> List[List[int]]:
     for i in range(0, len(intervals) - 1):
         print(intervals[i][1])
         if intervals[pointer][1] > intervals[i + 1][0]:
-            intervals[pointer][1] = max(intervals[pointer][1], intervals[i + 1][0])
-            intervals[pointer][1] = max(intervals[pointer][1], intervals[i + 1][1])
+            intervals[pointer][1] = max(intervals[pointer][1], intervals[i + 1][0], intervals[i + 1][1])
         else:
             pointer += 1
             intervals[pointer][0], intervals[pointer][1] = intervals[i+1][0], intervals[i+1][1]
